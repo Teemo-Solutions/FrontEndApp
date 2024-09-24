27cf945b60ebe41070a983d3dfa1c80915cc0d13
Vicente Quijandria <vquijandria@hotmail.com>
2024-09-24 02:15:14 -0500

    feat(user-login): implemented the input

---
+++ src/app/TeemoSolutions/components/user-login/user-login.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { EmailInputComponent } from "../../../public/components/email-input/email-input.component";
+import {
+  MatCard,
+  MatCardContent,
+  MatCardHeader,
+  MatCardSubtitle,
+  MatCardTitle,
+  MatCardTitleGroup
+} from "@angular/material/card";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+
+@Component({
+  selector: 'app-user-login',
+  standalone: true,
+  imports: [
+    MatCard,
+    MatCardTitle,
+    MatCardContent,
+    MatLabel,
+    MatFormField,
+    MatInput,
+    MatCardSubtitle,
+    MatCardTitleGroup,
+    MatCardHeader,
+    EmailInputComponent
+  ],
+  templateUrl: './user-login.component.html',
+  styleUrl: './user-login.component.css'
+})
+export class UserLoginComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ToolbarContentComponent} from "./public/components/toolbar-content/toolbar-content.component";
+import { UserLoginComponent} from "./TeemoSolutions/components/user-login/user-login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToolbarContentComponent],
+  imports: [RouterOutlet, ToolbarContentComponent, UserLoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
