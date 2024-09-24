608cddb01bedc349c984dbfe38672decff81e2af
Fernando Lizano <167820462+GuardianDeity@users.noreply.github.com>
2024-09-24 03:14:51 -0500

    feat(toolbar-content): import footer component

--- src/app/public/components/toolbar-content/toolbar-content.component.ts
+++ src/app/public/components/toolbar-content/toolbar-content.component.ts
@@ -3,6 +3,7 @@ import {MatIcon} from "@angular/material/icon";
 import {MatToolbar} from "@angular/material/toolbar";
 import {NgOptimizedImage} from "@angular/common";
 import {MatTab, MatTabGroup} from "@angular/material/tabs";
+import {FooterContentComponent} from "../footer-content/footer-content.component";
 
 @Component({
   selector: 'app-toolbar-content',
@@ -12,7 +13,8 @@ import {MatTab, MatTabGroup} from "@angular/material/tabs";
     MatToolbar,
     NgOptimizedImage,
     MatTabGroup,
-    MatTab
+    MatTab,
+    FooterContentComponent
   ],
   templateUrl: './toolbar-content.component.html',
   styleUrl: './toolbar-content.component.css'
