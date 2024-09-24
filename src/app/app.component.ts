519fb6110830a7508c972a1386b0c25659efd16a
Mitawarmi2000 <131271981+Mitawarmi2000@users.noreply.github.com>
2024-09-24 14:57:14 -0500

    feat(app): add options for toolbar into TypeScript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {ToolbarContentComponent} from "./public/components/toolbar-content/toolbar-content.component";
 import {FooterContentComponent} from "./public/components/footer-content/footer-content.component";
+import {MatCard, MatCardContent} from "@angular/material/card";
+import {MatAnchor} from "@angular/material/button";
+import {MatToolbar} from "@angular/material/toolbar";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToolbarContentComponent, FooterContentComponent],
+  imports: [RouterOutlet, ToolbarContentComponent, FooterContentComponent, MatCardContent, MatCard, MatAnchor, RouterLink, MatToolbar],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'FrontEndApp';
+
+  options = [
+    { path: '/home', title: 'Home'},
+    { path: '/about', title: 'About'},
+    { path: '/contact', title: 'Contact'},
+    { path: '/subscriptions', title: 'Subscriptions'},
+  ]
 }
