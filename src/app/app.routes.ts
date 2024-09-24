fba084d71e4a302b0e18f71ae0889677da450670
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-09-24 16:41:15 -0500

    feat: added principal configuration for show profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './public/pages/home/home.component';
 import { AboutComponent } from './public/pages/about/about.component';
 import {ContactComponent} from "./public/pages/contact/contact.component";
 import {SubscriptionsComponent} from "./public/pages/subscriptions/subscriptions.component";
+import { ProfileComponent } from "./public/pages/profile/profile.component"
 
 
 export const routes: Routes = [
@@ -11,6 +12,7 @@ export const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'subscriptions', component: SubscriptionsComponent },
+  { path: 'profile', component: ProfileComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' } // Redirecciona a 'home' por defecto
 ];
 
