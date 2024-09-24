a302c15daa6db13886c6df867bd773610f5e4bad
Fernando Lizano <167820462+GuardianDeity@users.noreply.github.com>
2024-09-24 02:41:03 -0500

    feat: add toolbar component for footer-content.component.ts

--- src/app/public/components/footer-content/footer-content.component.ts
+++ src/app/public/components/footer-content/footer-content.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatFormField} from "@angular/material/form-field";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-footer-content',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbar,
+    MatFormField,
+    MatIcon
+  ],
   templateUrl: './footer-content.component.html',
   styleUrl: './footer-content.component.css'
 })
