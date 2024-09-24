fc5a4d73f60011bf75e1a61dbfcb14ed9c177ccd
Mitawarmi2000 <131271981+Mitawarmi2000@users.noreply.github.com>
2024-09-24 18:11:58 -0500

    feat(routes): added profile and page not found.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,21 @@
+// src/app/app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './public/pages/home/home.component';
 import { AboutComponent } from './public/pages/about/about.component';
-import {ContactComponent} from "./public/pages/contact/contact.component";
-import {SubscriptionsComponent} from "./public/pages/subscriptions/subscriptions.component";
-
+import { ContactComponent } from './public/pages/contact/contact.component';
+import { SubscriptionsComponent } from './public/pages/subscriptions/subscriptions.component';
+import { ProfileComponent } from './public/pages/profile/profile.component';
+import { PageNotFoundComponent } from './public/pages/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'subscriptions', component: SubscriptionsComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' } // Redirecciona a 'home' por defecto
+  { path: 'profile', component: ProfileComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
