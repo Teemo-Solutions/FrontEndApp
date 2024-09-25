f146727a33d141f06373d88f4ae5be6ff053f76c
Fernando Lizano <167820462+GuardianDeity@users.noreply.github.com>
2024-09-25 00:58:03 -0500

    feat: add content for game services
    Co-authored-by: JoseRiega <u202211254@upc.edu.pe>

--- src/app/TeemoSolutions/services/game.service.ts
+++ src/app/TeemoSolutions/services/game.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
+  private apiUrl = 'http://localhost:3000/games';  // Reemplaza esto por tu endpoint real
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getGames(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
 }
