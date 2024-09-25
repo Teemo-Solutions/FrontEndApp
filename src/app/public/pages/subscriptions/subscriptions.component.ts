3c6fc420861348e1f40909d9387840efe0f51e27
Mitawarmi2000 <131271981+Mitawarmi2000@users.noreply.github.com>
2024-09-25 03:01:32 -0500

    feat(subscriptions): added subscriptions components.

--- src/app/public/pages/subscriptions/subscriptions.component.ts
+++ src/app/public/pages/subscriptions/subscriptions.component.ts
@@ -1,12 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {CurrencyPipe, NgForOf} from "@angular/common";
+import {MatButton} from "@angular/material/button";
+
+interface Plan {
+  nombre: string;
+  descripcion: string;
+  precio: number;
+  periodo: string;
+  caracteristicas: string[];
+}
 
 @Component({
   selector: 'app-subscriptions',
-  standalone: true,
-  imports: [],
   templateUrl: './subscriptions.component.html',
-  styleUrl: './subscriptions.component.css'
+  standalone: true,
+  imports: [
+    CurrencyPipe,
+    NgForOf,
+    MatButton
+  ],
+  styleUrls: ['./subscriptions.component.css']
 })
-export class SubscriptionsComponent {
+export class SubscriptionsComponent implements OnInit {
+  planes: Plan[] = [
+    {
+      nombre: 'Orbital Access',
+      descripcion: 'Plan Básico',
+      precio: 3,
+      periodo: 'Mensual',
+      caracteristicas: ['Recomendaciones personalizadas',  'Descuento en videojuegos (no incluye en lanzamiento)', 'Análisis básico de datos de juegos', 'Soporte básico']
+    },
+    {
+      nombre: 'Stellar Pass',
+      descripcion: 'Plan premium para usuarios avanzados',
+      precio: 9,
+      periodo: 'Mensual',
+      caracteristicas: ['Recomendaciones Personalizadas Avanzadas', 'Acceso Anticipados a videojuegos de los desarrolladores dentro de la plataforma',
+        'Análisis detallado de juegos', 'Soporte especializado', 'Descuento en videojuegos en Lanzamiento',
+        'Perfil de Gamer Reconocido (insignias, trofeos y más)']
+    },
+    {
+      nombre: 'Galactic Core',
+      descripcion: 'Plan Desarrollador Básico',
+      precio: 15,
+      periodo: 'Mensual',
+      caracteristicas: ['Capacidad de públicación: 10 videojuegos', 'Herramientas básicas de promoción de videojuegos', 'Análisis simplificado de rendimiento', 'Soporte técnico Estándar',
+        'Reparto de ingresos 90% / 10%']
+    }
+  ];
 
+  seleccionado: Plan | null = null;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  seleccionarPlan(plan: Plan): void {
+    this.seleccionado = plan;
+    console.log(`Plan seleccionado: ${plan.nombre} (${plan.precio})`);
+  }
+
+  verMas(plan: Plan): void {
+    console.log(`Ver más información sobre el plan ${plan.nombre}`);
+  }
 }
