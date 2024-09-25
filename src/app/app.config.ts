ec2f18fb1108de84ba23d4124de59b2141cfb2ea
Fernando Lizano <167820462+GuardianDeity@users.noreply.github.com>
2024-09-24 23:32:47 -0500

    refactor(app): update app component and service for the api

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,11 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';  // Asegúrate de importar provideHttpClient
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 
 import { routes } from './app.routes';
 
@@ -10,6 +15,10 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient()  // Añade HttpClient a los proveedores
+    provideHttpClient(), // Añade HttpClient a los proveedores
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
   ]
 };
