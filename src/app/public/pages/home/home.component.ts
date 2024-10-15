af6f6b76d1e1438f712b593dee2aab471c16ca8f
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-10-15 15:06:35 -0500

    feat(home): added the TranslateModule on the imports

--- src/app/public/pages/home/home.component.ts
+++ src/app/public/pages/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {GameListComponent} from "../../../TeemoSolutions/components/game-list/game-list.component";
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    GameListComponent
+    GameListComponent,
+    TranslateModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
