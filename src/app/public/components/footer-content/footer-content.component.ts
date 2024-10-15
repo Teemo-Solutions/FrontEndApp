3b41b723267dd7ba481196003f5aa26a2008055c
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-10-15 15:04:45 -0500

    feat(footer-content): added a function for the button to up

--- src/app/public/components/footer-content/footer-content.component.ts
+++ src/app/public/components/footer-content/footer-content.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import {MatToolbar} from "@angular/material/toolbar";
 import {MatFormField} from "@angular/material/form-field";
 import {MatIcon} from "@angular/material/icon";
+import {TranslateModule} from "@ngx-translate/core";
+import {MatInputModule} from "@angular/material/input";
 
 @Component({
   selector: 'app-footer-content',
@@ -9,11 +11,24 @@ import {MatIcon} from "@angular/material/icon";
   imports: [
     MatToolbar,
     MatFormField,
-    MatIcon
+    MatInputModule,
+    MatIcon,
+    TranslateModule
   ],
   templateUrl: './footer-content.component.html',
   styleUrl: './footer-content.component.css'
 })
 export class FooterContentComponent {
+  isButtonVisible = false;
 
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    // Mostrar el botón solo cuando el usuario haya hecho scroll hacia abajo
+    const scrollPosition =  document.documentElement.scrollTop || document.body.scrollTop || 0;
+    this.isButtonVisible = scrollPosition > 300; // Mostrar después de 300px
+  }
+
+  scrollToTop(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
