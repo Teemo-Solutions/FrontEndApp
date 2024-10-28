bdf38f85bd13b3dfe2182834a1d994f53623b2da
JoseRiega <u202211254@upc.edu.pe>
2024-10-27 23:14:50 -0500

    feat: added GameDetails imports

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,24 +1,24 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
-import { provideHttpClient } from '@angular/common/http';  // Asegúrate de importar provideHttpClient
+import { provideHttpClient } from '@angular/common/http';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { GameDetailsComponent } from './TeemoSolutions/components/game-details/game-details.component';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(), // Añade HttpClient a los proveedores
+    provideHttpClient(),
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
     MatButtonModule,
+    GameDetailsComponent,
   ]
 };
